b9adacde31a081a58939150fec61dcd0e26d2356
ldatciuk <ldatciuk@gmail.com>
2026-05-23 13:52:46 +0500

    Add files via upload

---
+++ audioop.py
@@ -0,0 +1,14 @@
+# audioop.py - заглушка для Alpine Linux
+def add(*args, **kwargs):
+    return b""
+
+
+def mul(*args, **kwargs):
+    return b""
+
+
+def reverse(*args, **kwargs):
+    return b""
+
+
+# Добавьте другие методы по мере необходимости
